0004bb879b0c34242511ebda050f93cc36c027dd
unknown <angie.morales.a403@gmail.com>
2022-10-28 16:11:23 -0500

    PRIMERO pasos

---
+++ JS/js5.js
@@ -0,0 +1,24 @@
+function operacion(op,numero,signumero2) {
+    let resultado = 0;
+   
+       switch(op){
+       case "+":
+           resultado=numero+signumero2;
+           break;
+       case "-":
+           resultado=numero-signumero2;
+           break;
+       case "*":
+           resultado=numero*signumero2;
+           break;
+       case "/":
+           resultado=numero/signumero2;
+           break;
+   
+   
+           
+       
+   } return resultado
+   }
+   
+   
